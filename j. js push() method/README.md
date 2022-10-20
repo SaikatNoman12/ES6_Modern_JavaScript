fc956c2162a5b04ebe2f0ae512c58aea979c4f66
saikatnoman12 <noommaa240@gmail.com>
2022-10-20 21:11:20 +0600

    add js array push()  method

---
+++ j. js push() method/README.md
@@ -0,0 +1,19 @@
+# JavaScript array push() method
+
+
+### How to use array push() method:
+
+#### Input:
+
+    const array = [ 1, 2, 3, 4, 5 ];
+
+    array.push(6,7,8,9);
+
+    console.log(array);
+
+#### Output:
+
+    [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+
---
+++ j. js push() method/app.js
@@ -0,0 +1,23 @@
+/* ==== JavaScript array push() method ==== */
+
+
+// Use array push() method:---------
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+array.push(8, 9, 10, 11, 12);
+
+console.log(array);  // output: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ];
+
+
+// Use array push() method:---------
+const array2 = [1, 2, 3, 4, 5, 6];
+
+const result = array2.push(7, 8, 9, 10);
+
+/*array2 output*/
+console.log(array2);  // [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+
+/*push output*/
+console.log(result);  // output: 10;
+
+
